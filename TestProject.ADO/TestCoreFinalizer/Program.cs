26f139fb1e2b8588b3c8d1c1abe41c6da3867284
Sergey.Bobr <Sergey.Bobr@softensity.com>
2021-08-30 14:04:00 +0300

    Add finalizer tests

---
+++ TestProject.ADO/TestCoreFinalizer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using TestFinolazer;
+
+namespace TestCoreFinalizer
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            var t = new TestClass();
+            t.Name = "Sergey";
+            var t2 = new Test2Class();
+            t2.Name = "Mikita";
+            Thread.Sleep( 100 );
+            GC.Collect();
+            GC.Collect();
+            GC.Collect();
+            t2.Name = "";
+            GC.Collect();
+            GC.Collect();
+            GC.SuppressFinalize( t2 );
+            GC.WaitForPendingFinalizers();
+            Console.WriteLine( "dmsahfajkshdfkashbdkjmhasldkhj" );
+            for ( int index = 0; index < 1000000000000000; index++ )
+            {
+                var tt = new TestClass();
+                GC.Collect();
+            }
+        }
+    }
+}
---
+++ TestProject.ADO/TestFinolazer/TestClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace TestFinolazer
+{
+    public class TestClass
+    {
+        public string Name { get; set; }
+        ~TestClass()
+        {
+            Debug.WriteLine( "finalize" );
+            Thread.Sleep(100000);
+            Debug.WriteLine("finalized");
+        }
+    }
+    public class Test2Class
+    {
+        public string Name { get; set; }
+        ~Test2Class()
+        {
+            Debug.WriteLine( "finalize2" );
+            Thread.Sleep( 100000 );
+            Debug.WriteLine( "finalized2" );
+        }
+    }
+}
