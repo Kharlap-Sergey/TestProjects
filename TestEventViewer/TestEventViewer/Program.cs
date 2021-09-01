4dca85862021e4dcc6a3d73b0a20c0e6b363fa1b
Sergey.Bobr <Sergey.Bobr@softensity.com>
2021-09-01 09:12:45 +0300

    Added test event viewer

---
+++ TestEventViewer/TestEventViewer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEventViewer
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            try
+            {
+                EventLog.WriteEntry( "WebOps - Shopify Service", "Test", EventLogEntryType.Information );
+                Console.WriteLine("Tested successfully!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
