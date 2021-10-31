2f0c20a364b323d3c2e996d8782f0bd493b5cb00
Siarhei Kharlap <sergeugoodgood@gmail.com>
2021-10-31 17:51:55 +0300

    Add 4(7) lab

---
+++ OZI-labs/lab4(7)/AssymetricAlgo/AssymetricAlgo/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace AssymetricAlgo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plainText = new System.Windows.Forms.TextBox();
+            this.encryptedText = new System.Windows.Forms.TextBox();
+            this.decryptedText = new System.Windows.Forms.TextBox();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // plainText
+            // 
+            this.plainText.Location = new System.Drawing.Point(27, 37);
+            this.plainText.Name = "plainText";
+            this.plainText.Size = new System.Drawing.Size(100, 20);
+            this.plainText.TabIndex = 0;
+            // 
+            // encryptedText
+            // 
+            this.encryptedText.Location = new System.Drawing.Point(27, 150);
+            this.encryptedText.Name = "encryptedText";
+            this.encryptedText.Size = new System.Drawing.Size(100, 20);
+            this.encryptedText.TabIndex = 1;
+            // 
+            // decryptedText
+            // 
+            this.decryptedText.Location = new System.Drawing.Point(27, 266);
+            this.decryptedText.Name = "decryptedText";
+            this.decryptedText.Size = new System.Drawing.Size(100, 20);
+            this.decryptedText.TabIndex = 2;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(370, 37);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(379, 375);
+            this.richTextBox.TabIndex = 3;
+            this.richTextBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.decryptedText);
+            this.Controls.Add(this.encryptedText);
+            this.Controls.Add(this.plainText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox plainText;
+        private System.Windows.Forms.TextBox encryptedText;
+        private System.Windows.Forms.TextBox decryptedText;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ OZI-labs/lab4(7)/AssymetricAlgo/AssymetricAlgo/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AssymetricAlgo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public byte[] encryptedData;
+        public byte[] decryptedData;
+
+        static string KeyContainerName = "MyKeyContainer";
+        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(2048, RSAPersistKeyInCsp(KeyContainerName));
+        UnicodeEncoding ByteConverter = new UnicodeEncoding();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //RSAPersistKeyInCsp(KeyContainerName);
+                richTextBox.Clear();
+                byte[] dataToEncrypt = ByteConverter.GetBytes(plainText.Text);
+                encryptedData = RSAEncrypt(dataToEncrypt, RSA.ExportParameters(false), false);
+                encryptedText.Text = ByteConverter.GetString(encryptedData);
+                var p = RSA.ExportParameters(true);
+                richTextBox.Text += "Текущий размер ключа:" + RSA.KeySize + " Модуль:" + p.Modulus[0].ToString() + " Показатель степени:" + p.Exponent[0].ToString();
+                richTextBox.Text += "\n Размер ключей от " + RSA.LegalKeySizes[0].MinSize.ToString() + " до " + RSA.LegalKeySizes[0].MaxSize.ToString();
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Encryption failed.");
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (richTextBox.Lines.Length > 5)
+            {
+                richTextBox.Clear();
+            }
+
+            decryptedData = RSADecrypt(encryptedData, RSA.ExportParameters(true), false);
+            decryptedText.Text = ByteConverter.GetString(decryptedData);
+            var p = RSA.ExportParameters(true);
+            richTextBox.Text += "\n P: ";
+            for (int i = 0; i < 50; i++)
+            {
+                richTextBox.Text += p.P[i];
+            }
+            richTextBox.Text += "\n Q: ";
+            for (int i = 0; i < 50; i++)
+            {
+                richTextBox.Text += p.Q[i];
+            }
+            richTextBox.Text += "\n N: ";
+            for (int i = 0; i < 50; i++)
+            {
+                richTextBox.Text += p.Modulus[i];
+            }
+
+            richTextBox.Text += "\n e: ";
+            for (int i = 0; i < p.Exponent.Length; i++)
+            {
+                richTextBox.Text += p.Exponent[i];
+            }
+
+            richTextBox.Text += "\n d: ";
+            for (int i = 0; i < 50; i++)
+            {
+                richTextBox.Text += p.D[i];
+            }
+            richTextBox.Text += "\nlength " + p.P.Length;
+
+        }
+
+
+        public static CspParameters RSAPersistKeyInCsp(string ContainerName)
+        {
+            try
+            {
+                CspParameters cspParams = new CspParameters();
+                cspParams.Flags = CspProviderFlags.UseDefaultKeyContainer;
+                cspParams.KeyContainerName = ContainerName;
+                Console.WriteLine("The RSA key was persisted in the container, \"{0}\".",
+                    ContainerName);
+
+                return cspParams;
+            }
+            catch (CryptographicException e)
+            {
+                MessageBox.Show(e.Message);
+                return null;
+            }
+
+        }
+
+        static public byte[] RSAEncrypt(byte[] DataToEncrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(2048, RSAPersistKeyInCsp(KeyContainerName)))
+                {
+                    RSA.ImportParameters(RSAKeyInfo);
+                    encryptedData = RSA.Encrypt(DataToEncrypt, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+
+        }
+
+        public byte[] RSADecrypt(byte[] DataToDecrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(2048, RSAPersistKeyInCsp(KeyContainerName)))
+                {
+                    RSA.ImportParameters(RSAKeyInfo);
+                    decryptedData = RSA.Decrypt(DataToDecrypt, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+
