022cda70e7677d852e06b73fcc086f78ae0dd787
siarhei <siarhie@gmail>
2022-03-18 21:44:29 +0300

    Add lab 3

---
+++ term-6/DB-labs/ЛАБ_РАБ/DB/Lab3/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.getProduct_Btn = new System.Windows.Forms.Button();
+            this.products_dataGridView = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.products_dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // getProduct_Btn
+            // 
+            this.getProduct_Btn.Location = new System.Drawing.Point(23, 384);
+            this.getProduct_Btn.Name = "getProduct_Btn";
+            this.getProduct_Btn.Size = new System.Drawing.Size(259, 23);
+            this.getProduct_Btn.TabIndex = 1;
+            this.getProduct_Btn.Text = "Get Products";
+            this.getProduct_Btn.UseVisualStyleBackColor = true;
+            this.getProduct_Btn.Click += new System.EventHandler(this.getProduct_Btn_Click);
+            // 
+            // products_dataGridView
+            // 
+            this.products_dataGridView.AllowUserToAddRows = false;
+            this.products_dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.products_dataGridView.Location = new System.Drawing.Point(23, 46);
+            this.products_dataGridView.Name = "products_dataGridView";
+            this.products_dataGridView.RowTemplate.Height = 25;
+            this.products_dataGridView.Size = new System.Drawing.Size(240, 303);
+            this.products_dataGridView.TabIndex = 2;
+            this.products_dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(325, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "test table";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(471, 387);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(469, 277);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(469, 322);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.products_dataGridView);
+            this.Controls.Add(this.getProduct_Btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.products_dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button getProduct_Btn;
+        private System.Windows.Forms.DataGridView products_dataGridView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ term-6/DB-labs/ЛАБ_РАБ/DB/Lab3/Logic.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Lab3
+{
+    partial class Form1
+    {
+        public static string ConnectionString { get; set; } =
+            "Data Source=WSA-220-71;Initial Catalog=db2;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public DataTable GetProducts()
+        {
+            using var connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            var command = @"SELECT TOP (1000) [ID]
+      ,[NAME]
+      ,[PRICE]
+      ,[PRODUCT_CATEGORY_ID]
+      ,[IS_DELETED]
+  FROM [db2].[dbo].[PRODUCTS]";
+            var adapter = new SqlDataAdapter(command, connection);
+            DataSet products = new DataSet();
+            adapter.Fill(products, "Products");
+
+            return products.Tables[0];
+        }
+
+        public DataRow AddRowToProducts(string productName, ushort price)
+        {
+            using var connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            var command = @"SELECT TOP (1000) [ID]
+      ,[NAME]
+      ,[PRICE]
+      ,[PRODUCT_CATEGORY_ID]
+      ,[IS_DELETED]
+  FROM [db2].[dbo].[PRODUCTS]";
+            var adapter = new SqlDataAdapter(command, connection);
+            DataSet productsS = new DataSet();
+            adapter.Fill(productsS, "PRODUCTS");
+
+            var products = productsS.Tables["PRODUCTS"];
+
+            var row = products.NewRow();
+            row["Name"] = productName;
+            row["PRICE"] = price;
+            row["PRODUCT_CATEGORY_ID"] = 1;
+
+            products.Rows.Add(row);
+
+            new SqlCommandBuilder(adapter);
+            adapter.Update(products);
+
+            return row;
+        }
+
+        public DataTable CreateTestTable()
+        {
+            DataSet products = new DataSet("Test");
+            DataTable empTable = products.Tables.Add("Employees1");
+            DataColumn empID = empTable.Columns.Add("EmployeeID", typeof(Int32));
+            empID.AutoIncrement = true;
+            empID.AutoIncrementSeed = 200;
+            empID.AutoIncrementStep = 3;
+            empTable.Columns.Add("LName", typeof(string));
+            empTable.Columns.Add("FName", typeof(string));
+            empTable.Columns.Add("MName", typeof(string));
+            empTable.PrimaryKey = new DataColumn[] { empTable.Columns["EmployeeID"] };
+
+            var newRow = CreateTestRow(empTable , "sadfasdf", "assdfasdf", "adfasdf");
+            var newRow1 = CreateTestRow(empTable , "1sadfasdf", "1assdfasdf", "adfasdf");
+            var newRow2 = CreateTestRow(empTable , "2sadfasdf", "2assdfasdf", "adfasdf");
+
+            empTable.Rows.Add(newRow);
+            empTable.Rows.Add(newRow1);
+            empTable.Rows.Add(newRow2);
+
+            return products.Tables[0];
+        }
+
+        private DataRow CreateTestRow(DataTable newTable,string a, string b, string c)
+        {
+            var newRow = newTable.NewRow();
+
+            newRow["LName"] = a;
+            newRow["FName"] = b;
+            newRow["MName"] = c;
+
+            return newRow;
+        }
+    }
+}
