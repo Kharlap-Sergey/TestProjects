4b0862ec7cf318a57ae1022a0456c96eb70d2508
Siarhei Kharlap <sergeugoodgood@gmail.com>
2022-03-19 14:15:20 +0300

    Add lab 4

---
+++ term-6/DB-labs/ЛАБ_РАБ/DB/Lab4/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.products_dataGridView = new System.Windows.Forms.DataGridView();
+            this.productCategories_dataGridView = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.products_dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productCategories_dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // products_dataGridView
+            // 
+            this.products_dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.products_dataGridView.Location = new System.Drawing.Point(0, 0);
+            this.products_dataGridView.Name = "products_dataGridView";
+            this.products_dataGridView.RowTemplate.Height = 25;
+            this.products_dataGridView.Size = new System.Drawing.Size(479, 150);
+            this.products_dataGridView.TabIndex = 0;
+            // 
+            // productCategories_dataGridView
+            // 
+            this.productCategories_dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.productCategories_dataGridView.Location = new System.Drawing.Point(0, 270);
+            this.productCategories_dataGridView.Name = "productCategories_dataGridView";
+            this.productCategories_dataGridView.Size = new System.Drawing.Size(354, 150);
+            this.productCategories_dataGridView.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(500, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "apply";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(500, 119);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(78, 31);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "reject";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(417, 270);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(78, 31);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "apply";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(417, 389);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(78, 31);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "reject";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(647, 389);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(619, 222);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 94);
+            this.listBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.productCategories_dataGridView);
+            this.Controls.Add(this.products_dataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.products_dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productCategories_dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView products_dataGridView;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView productCategories_dataGridView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ term-6/DB-labs/ЛАБ_РАБ/DB/Lab4/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        public static string ConnectionString { get; set; } =
+            @"Data Source=SIARHEI-KHARLAP\SQLEXPRESS;Initial Catalog=DB2;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private DataSet ds;
+        private SqlDataAdapter productsAdapter;
+        private SqlDataAdapter productCategoriesAdapter;
+        private SqlConnection conn;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(ConnectionString);
+
+            productsAdapter = new SqlDataAdapter(
+                "Select * from Products",
+                conn
+            );
+
+            productCategoriesAdapter = new SqlDataAdapter(
+                "select * from PRODUCT_CATEGORIES",
+                conn
+            );
+
+            ds = new DataSet();
+            productsAdapter.Fill(ds, "Products");
+            productCategoriesAdapter.Fill(ds, "ProductCategories");
+
+            productCategories_dataGridView.DataSource = ds.Tables["ProductCategories"];
+            products_dataGridView.DataSource = ds.Tables["Products"];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlCommandBuilder builder = new SqlCommandBuilder(productsAdapter);
+            var products = ds.Tables["Products"];
+            var count = productsAdapter.Update(products);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var products = ds.Tables["Products"];
+            products.RejectChanges();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var products = ds.Tables["ProductCategories"];
+            products.RejectChanges();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlCommandBuilder builder = new SqlCommandBuilder(productCategoriesAdapter);
+            var products = ds.Tables["ProductCategories"];
+            var count = productCategoriesAdapter.Update(products);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var products = ds.Tables["Products"];
+            var prodCat = ds.Tables["ProductCategories"];
+            DataRelation relat = new DataRelation("Rel1",
+                prodCat.Columns["id"],
+                products.Columns["Product_category_id"]);
+            ds.Relations.Add(relat);
+
+            foreach (DataRow row in prodCat.Rows)
+            {
+                listBox1.Items.Add(row["name"]);
+                DataRow[] childRows = row.GetChildRows(relat); // Получение связанных данных
+                foreach (DataRow childRow in childRows) listBox1.Items.Add("    " + childRow["name"]);
+            }
+
+        }
+    }
+}
